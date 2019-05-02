bdbb198e56a3e4a0ddb050589c248fd17368cbf3
sivadeveloper-ssm <mssiva86@gmail.com>
2019-05-02 12:28:35 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  baseUrl : 'http://wafstcaps03:9080/smswebservice/api/'
+  production: false
 };
 
 /*
